310d6d48444a4e82628f1705b93814004f48a587
Mike Street <mike@liquidlight.co.uk>
2021-11-11 22:22:35 +0000

    Initial commit

---
+++ ext_emconf.php
@@ -0,0 +1,20 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+	'title' => 'Shortcodes',
+	'description' => 'Wordpress-style shortcodes for page content',
+	'category' => 'plugin',
+	'author' => 'Mike Street',
+	'author_email' => 'mike@liquidlight.co.uk',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'version' => '0.1.0',
+	'constraints' => [
+		'depends' => [
+			'typo3' => '9.5.0-9.5.99',
+		],
+		'suggests' => [],
+	],
+];
