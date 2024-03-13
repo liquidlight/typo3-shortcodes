3331f1087a5e69fee995fae37bf968559b688267
Mike Street <mike@liquidlight.co.uk>
2024-03-13 11:45:38 +0000

    fix: Re-instate ex_emconf as tailor needs it

---
+++ ext_emconf.php
@@ -0,0 +1,20 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+	'title' => 'Shortcodes',
+	'description' => 'Wordpress-style shortcodes for TYPO3',
+	'category' => 'plugin',
+	'author' => 'Mike Street',
+	'author_email' => 'mike@liquidlight.co.uk',
+	'state' => 'stable',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'clearCacheOnLoad' => 0,
+	'version' => '1.8.0',
+	'constraints' => [
+		'depends' => [
+			'typo3' => '11.5.0-12.99.99',
+		],
+		'suggests' => [],
+	],
+];
