e1698fe2dfcf468fde159cd563dd0216feaf279c
Mike Street <mikestreety@gmail.com>
2025-07-16 07:58:36 +0100

    build: Add TYPO3 13 support

---
+++ UPCOMING.md
@@ -0,0 +1,5 @@
+# Minor
+
+#### Depndencies
+
+- Add support for TYPO3 13 ([#36](https://github.com/liquidlight/typo3-shortcodes/issues/36))
--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
 	'version' => '2.1.2',
 	'constraints' => [
 		'depends' => [
-			'typo3' => '11.5.0-12.99.99',
+			'typo3' => '11.5.0-13.99.99',
 		],
 		'suggests' => [],
 	],
